3baae3d838feb48b10c74452f05a39cd8def26bd
raghavarora19 <raghavarora911@gmail.com>
2018-10-29 02:36:16 -0400

    final

--- fileclient.py
+++ fileclient.py
@@ -2,39 +2,40 @@ import socket
 import argparse
 
 
-
-
-def get(directory, action, port):
-    request= "GET / HTTP/1.0\r\n" + directory + "\r\n\r\n"
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    request_new =request.split("/")
-    if request_new[0] == " ":
-      s.connect(("127.0.0.1", port))
-      s.sendall(request.encode())
-    else:
-      def get(directory, action, port):
-       request = "GET / HTTP/1.0\r\n" + directory + "\r\n\r\n"
-       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       s.connect(("127.0.0.1", port))
-       s.sendall(request.encode())
-def post(directory, data, portx):
-    request = "POST / HTTP/1.0\r\n" + directory + " " + data + "\r\n\r\n"
+def get(directory, port):
+    request = "GET / HTTP/1.0\r\n" + directory + "\r\n"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("127.0.0.1", port))
     s.sendall(request.encode())
+    result = s.recv(4096)
+    incoming = result.decode()
+    #split
+    print(incoming)
+
+
+def post(directory, port,data):
+    request = "POST / HTTP/1.0\r\n" + directory + "\r\n" + data + "\r\n"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("127.0.0.1", port))
+    s.sendall(request.encode())
+    result = s.recv(4096)
+    response = result.decode()
+    # split
+    print(response)
+
 
 def main():
+    argParser = argparse.ArgumentParser(description='Socket based HTTP fileserver')
+    argParser.add_argument('req_type', type=str, help="GET/POST")
+    argParser.add_argument("-d", '--directory', type=str, action="store", help="Set directory path", default='/')
+    argParser.add_argument("-data", '--data', type=str, action="store", nargs="+", help="POST DATA")
+    argParser.add_argument("-p", '--port', action="store", help="Set server port", type=int, default=8080)
+    args = argParser.parse_args()
 
-        argParser = argparse.ArgumentParser(description='Socket based HTTP fileserver')
-        argParser.add_argument('req_type', type=str, help="GET/POST")
-        argParser.add_argument('action', type=str, help="specific action user is doing")
-        argParser.add_argument("p", action="store", dest="port", help="Set server port", type=int, default=8080)
-        argParser.add_argument("data", type= str, action="store", nargs="+", help="Echo debugging mesages", default=False)
-        argParser.add_argument("d", action="store", dest="directory", help="Set directory path", default='./')
-        args = argParser.parse_args()
+    if args.req_type == "get" and "GET":
+        get(args.directory, args.port)
+    elif args.re_type == "post" and "POST":
+        post(args.directory,args.port, ''.join(args.data))
 
-        if args.req_type == "get" and "GET" :
-            get(args.directory, args.action, args.port)
-        if args.re_type == "post" and "POST":
-            post(args.directory, args.action, args.port, ''.join(args.data))
+
 main()
--- fileserver.py
+++ fileserver.py
@@ -6,47 +6,100 @@ import sys
 import threading
 
 sys.path.extend(["./"])
-def server_run(host, port, dir):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(10)
 
+def server(host, port, dir):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen(10)
     while True:
-        (csock, addr) = server_socket.accept()
-        #threading.Thread(target=handle_client, args=(conn, addr, dir)).start()
-        data_recv = csock.recv(1024)
-        decode_data=data_recv.decode()
-        csock.sendall(decode_data.encode("utf-8"))
+        (csock, address) = sock.accept()
+        threading.Thread(target=requesthandler(), args=(csock, address, dir)).start()
+        #csock.shutdown(socket.SHUT_WR)
+    sock.close()
 
-        csock.shutdown(socket.SHUT_WR)
-        csock.close()
+def statuscode(statuscode, message,content):
+
+    header = "HTTP /1.0 " + statuscode + " " + message + " "+ content + " "
+    return header
 
 
-def handle_client(conn, addr, dir):
-    if args.debugging:
-        print('Handle New client from', addr)
+
+def requesthandler(csock, address,dir):
+    if args.debug:
+        print("Handles Client:",address)
         while True:
-            if (data):
-             data = conn.recv(2048)
-             data = data.decode("utf-8")
-            if (not data):
-             break
+             data_recv = csock.recvall()
+             decode_data = data_recv.decode("utf-8")
+             if not decode_data:
+                break
+             data = decode_data.split('\r\n')
+             rtype= data[0]
+             path = data[1]
+             if 'POST' in decode_data:
+                 in_data = data[3]
+             if '..' in path:
+                 if args.debug:
+                     print("Access Denied",path)
+                 header_to_return = statuscode(400,'Access Denied','')
+             else :
+                 if not dir.endswith("/"):
+                     dir = dir + "/"
+                 path = (dir + path).replace("//","/")
+             if rtype =="GET":
+                if path.endswith("/"):
+                  if args.debug:
+                   print("GET REQUEST ->DIRECTORY:",path)
+                   file =os.listdir(path)
+                   header_to_return = statuscode(200,json.dumps(files).encode("ascii"),"Content-Type: application/json")
+                  else :
+                    if os.path.exists(path):
+                     if args.debug:
+                      print("File",path)
+                      header_to_return = statuscode(200,'','')
+                      type = magic.from_file(path, mime=True)
+                      header_to_return = statuscode(200,'','')
+                     if "text" in type:
+                      with open(path,'r') as f1:
+                      file_content = f1.read()
+                      header_to_return += json.dumps("Content-Length"+str(len(file_content))) + "\r \n"
+                      header_to_return += file_content +"\r\n"
+                      else :
+                       with open(path, 'rb') as f1:
+                       file_content = f1.read()
+                       header_to_return += json.dumps("Content-Length" + str(len(file_content))) + "\r \n"
+                       header_to_return += file_content + "\r\n"
 
-             (method, path, query, body, headers) = parseRequest(data)
-             if (args.debugging):
-                 print(method, path, body, headers)
+                        #if "Content-Disposition" in decode_data:
+                        #header_to_return +=  "Content-Disposition"
+                        #elif "inline" in query:
+
+                  else :
+                   header_to_return = statuscode(404,"".encode("ascii"),"")
+
+                    except: OSError as e:
+                            if args.debug:
+                                print(e)
+                            header_to_return = http(400, e.strerror)
 
 
 
-def parse_request(port): #include the parsing from different server
 
 
 
-        parser = argparse.ArgumentParser(description='Socket based HTTP fileserver')
-        parser.add_argument("-p", action="store", dest="port", help="Set server port", type=int, default=8080)
-        parser.add_argument("-v", action="store_true", dest="debugging", help="Echo debugging mesages", default=False)
-        parser.add_argument("-d", action="store", dest="directory", help="Set directory path", default='./')
-    args = parser.parse_args()
 
-    if (args.debugging):
-        server_run('', args.port, args.directory)
+
+
+
+
+
+
+
+
+parse = argparse.ArgumentParser(description='HTTP FILE SERVER')
+parse.add_argument("-p", action="store", dest="port", help="Set server port", type=int, default=8080)
+parse.add_argument("-d", action="store", dest="directory", help="Set directory path", default='./')
+parse.add_argument('-v', "--debug", dest="debug", action='store_true', default=False,help="Prints Debugging Messages")
+args = parse.parse_args()
+server('127.0.0.1', args.port, args.directory)
+
+
